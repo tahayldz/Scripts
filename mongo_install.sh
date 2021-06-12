68461c1880ffcc73d6da08783d1d674b8d18dc15
Taha YILDIZ <44941791+tahayldz@users.noreply.github.com>
2021-06-12 22:34:24 +0300

    Create mongo_install.sh

---
+++ mongo_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl daemon-reload
+sudo systemctl status mongod
